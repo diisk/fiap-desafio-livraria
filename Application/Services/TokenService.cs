8370d0f7bb2740ffc2ecd5bde3ea36ec78aa25ee
DiisK <diisk@hotmail.com>
2024-09-20 04:22:58 -0300

    feat: added tokenservice but unfinished

---
+++ Application/Services/TokenService.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration configuration;
+
+        public TokenService(IConfiguration configuration) {
+            this.configuration = configuration;
+        }
+        public string GetToken(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    [Table("Usuario")]
+    public class Usuario:EntityBase
+    {
+        public required string Email { get; set; }
+        public required string Senha { get; set; }
+
+
+    }
+}
